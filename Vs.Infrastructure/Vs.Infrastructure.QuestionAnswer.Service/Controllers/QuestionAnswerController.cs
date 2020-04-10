540c6cf0f7c8763bec852f4bb92964e2f8679a67
sjefvanleeuwen <@sjefvanleeuwen>
2020-04-10 10:17:15 +0200

    Initial

---
+++ Vs.Infrastructure/Vs.Infrastructure.QuestionAnswer.Service/Controllers/QuestionAnswerController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Vs.Infrastructure.QuestionAnswer.Service.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class QuestionAnswerController : ControllerBase
+    {
+    }
+}
